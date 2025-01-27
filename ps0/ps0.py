48b0d69ca3c0e3b7140fa7902f02b16e3ceda152
Noureddine Idali <nidali673@gmail.com>
2025-01-27 16:20:03 +0100

    Adding the problem sets to the repository

---
+++ ps0/ps0.py
@@ -0,0 +1,17 @@
+# Noureddine Idali          26/01/2025
+
+
+# Problem set 0:
+
+
+import math
+import numpy
+
+x = int(input("Enter a number x: ")) # we could use the int() to transform the user input from a string to an integer 
+print("x = ", x + 2)
+y = input("Enter a number y: ")
+print("x**y = ", int(x)**int(y))
+print("log(x) = ", math.log(int(x),2))
+print("log(x) = ", numpy.log2(int(x)))
+
+
